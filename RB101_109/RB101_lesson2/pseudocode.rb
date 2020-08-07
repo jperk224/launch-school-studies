4353d745775c97837b157e787aa032073a630194
Jeff Perkinson <jmperkinson@gmail.com>
2020-08-07 17:02:50 -0400

    Add .gitignore and RB101 lesson 2 pieces.

---
+++ RB101_109/RB101_lesson2/pseudocode.rb
@@ -0,0 +1,32 @@
+# a method that returns the sum of two integers
+=begin
+START
+# given two integers, num1 and num2
+SET sum = num1 + num2
+PRINT sum
+END
+=end
+
+# A method that takes an array of strings, and returns a string that is all
+# those strings concatenated together
+=begin
+START
+# given an array of strings, string_array
+SET string equal to an empty string
+iterate over string_array
+append each individual element in the array to string
+PRINT string
+END
+=end
+
+# a method that takes an array of integers, and retuns 
+# a new array with every other element
+=begin
+START
+# given an array of integers, integer_array
+SET skip_array to an empty array
+iterate over integer_array
+if element's index is of step 2 (e.g. divisible by 2 with no remainder)
+  add element to skip_array
+END
+=end
