2812b6a633a17fc78b1dd5a4f289e755ab4be489
Jeff Perkinson <jmperkinson@gmail.com>
2020-08-09 19:57:30 -0400

    Add user capture for all 3 main inputs.

--- RB101_109/RB101_lesson2/payment_calculator.rb
+++ RB101_109/RB101_lesson2/payment_calculator.rb
@@ -48,26 +48,30 @@ def integer?(str)
 end
 
 def valid_number?(str)
-  float?(str) || integer?(str)
+  (float?(str) || integer?(str))
 end
 
 # get numerical input from the user, validate it
 # and return it to store in a variable
-def get_user_number(type, prompt)
+def get_user_number(type, prompt, error_message=MESSAGES["default_bad_entry"])
   user_input = ''
   loop do
     prompt(prompt)
     user_input = gets.chomp
     valid_entry = false
     case type
-    when 'loan'
-      if !valid_number?(user_input) || user_input.to_f <= 0
-        prompt(MESSAGES["bad_loan_amount"])
+    when 'loan', 'duration'
+      if !valid_number?(user_input) || !((user_input.to_i > 0) || (user_input.to_f > 0))
+        prompt(error_message)
       else
         valid_entry = true
       end
     when 'apr'
-    when 'loan_duration'
+      if !valid_number?(user_input) || !((user_input.to_i >= 0) || (user_input.to_f >= 0))
+        prompt(error_message)
+      else
+        valid_entry = true
+      end
     end
     break if valid_entry
   end
@@ -91,7 +95,11 @@ end
 prompt("#{MESSAGES["welcome"]} #{name}!")
 
 loop do # main loop
-  amount = get_user_number('loan', MESSAGES["get_loan_amount"])
+  amount = get_user_number('loan', MESSAGES["get_loan_amount"], MESSAGES["bad_loan_entry"])
   puts amount
+  apr = get_user_number('apr', MESSAGES["get_apr"], MESSAGES["bad_apr_entry"])
+  puts apr
+  duration = get_user_number('duration', MESSAGES["get_duration"], MESSAGES["bad_duration_entry"])
+  puts duration
   break
 end
