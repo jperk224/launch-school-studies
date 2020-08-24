459992d459cbecf028e6d2b1de9fd3052976d913
Jeff Perkinson <jmperkinson@gmail.com>
2020-08-24 15:46:11 -0400

    Add player places piece method.

--- RB101_109/RB101_lesson6/tic_tac_toe.rb
+++ RB101_109/RB101_lesson6/tic_tac_toe.rb
@@ -1,3 +1,12 @@
+# Game constants
+INITIAL_MARKER = ' '
+PLAYER_MARKER = 'X'
+COMPUTER_MARKER = 'O'
+
+def prompt(message)
+  puts "=> #{message}"
+end
+
 # Display the board
 def display_board(board)
   puts ""
@@ -18,10 +27,29 @@ end
 # Initialize the board
 def initialize_board
   new_board = {}
-  (1..9).each { |num| new_board[num] = ' ' }
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
   new_board
+end
+
+def empty_squares(brd)
+  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
+end
+
+def player_places_piece!(brd)
+  square = ''
+  loop do
+    prompt "Choose a square (#{empty_squares(brd).join(', ')}):"
+    square = gets.chomp.to_i
+    break if empty_squares(brd).include?(square)
+    prompt "Sorry, that is not a valid choice."
+  end
+  brd[square] = PLAYER_MARKER
 end
 
 # main game
 board = initialize_board
+display_board(board)
+
+player_places_piece!(board)
+# puts board.inspect
 display_board(board)
