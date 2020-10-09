4451ff8c8d237668f0242e657b0b080dae93bea4
Jeff Perkinson <jmperkinson@gmail.com>
2020-10-09 05:34:12 -0400

    Add initial OOP rps pre bonus.

---
+++ RB120/lesson2/rps/rps.md
@@ -0,0 +1,27 @@
+Rock paper scissors is a 2 person game
+Each person chooses to present an item - rock, paper, or scissors
+rock > scissors
+scissors > paper
+paper > rock
+The person with the winning item wins!
+If the players choose the same item, it's a tie
+
+# Major nouns and verbs
+- Player 1
+- Player 2
+- Move (Rock, Paper, Scissors are different states)
+
+- Selection (associated with each player)
+- Compare
+- Present winner
+- Tie
+
+Player
+- choose
+
+Move
+Rule (??) (Move hierarchy)
+
+- compare
+
+
---
+++ RB120/lesson2/rps/rps.rb
@@ -0,0 +1,141 @@
+# Game classes
+class Move
+  VALUES = ['rock', 'paper', 'scissors']
+
+  def initialize(value)
+    @value = value
+  end
+
+  def scissors?
+    @value == 'scissors'
+  end
+
+  def rock?
+    @value == 'rock'
+  end
+
+  def paper?
+    @value == 'paper'
+  end
+
+  def to_s
+    @value
+  end
+
+  def >(other_move)
+    (rock? && other_move.scissors?) ||
+      (paper? && other_move.rock?) ||
+      (scissors? && other_move.paper?)
+  end
+
+  def <(other_move)
+    (rock? && other_move.paper?) ||
+      (paper? && other_move.scissors?) ||
+      (scissors? && other_move.rock?)
+  end
+end
+
+class Player
+  attr_accessor :move, :name
+
+  def initialize
+    @move = nil
+    set_name
+  end
+
+  def human?
+    @player_type == :human
+  end
+end
+
+class Human < Player
+  def set_name
+    n = nil
+    loop do
+      puts "What's your name?"
+      n = gets.chomp
+      break unless n.empty?
+      puts "Sorry, please enter a value."
+    end
+    self.name = n
+  end
+
+  def choose
+    choice = nil
+    loop do
+      puts "Please choose rock, paper, or scissors:"
+      choice = gets.chomp
+      break if Move::VALUES.include?(choice)
+      puts "Sorry, invalid choice."
+    end
+    self.move = Move.new(choice)
+  end
+end
+
+class Computer < Player
+  def set_name
+    self.name = ['R2D2', 'Hal', 'Chappie', 'Sonny', 'Number 5'].sample
+  end
+
+  def choose
+    self.move = Move.new(Move::VALUES.sample)
+  end
+end
+
+# Orchestration Engine
+class RPSGame
+  attr_accessor :human, :computer
+
+  def initialize
+    @human = Human.new
+    @computer = Computer.new
+  end
+
+  def display_welcome_message
+    puts "Welcome to Rock, Paper Scissors!"
+  end
+
+  def display_goodbye_message
+    puts "Thanks for playing Rock, Paper, Scissors.  Good bye!"
+  end
+
+  def display_moves
+    puts "#{human.name} chose #{human.move}"
+    puts "#{computer.name} chose #{computer.move}"
+  end
+
+  def display_winner
+    if human.move > computer.move
+      puts "#{human.name} won!"
+    elsif human.move < computer.move
+      puts "#{computer.name} won!"
+    else
+      puts "It's a tie!"
+    end
+  end
+
+  def play_again?
+    answer = nil
+    loop do
+      puts "Would you like to play again (y/n)?"
+      answer = gets.chomp.downcase
+      break if %w(y n).include? answer
+    end
+    return true if answer == 'y'
+    return false if answer == 'n'
+  end
+
+  def play
+    loop do
+      display_welcome_message
+      human.choose
+      computer.choose
+      display_moves
+      display_winner
+      break unless play_again?
+    end
+    display_goodbye_message
+  end
+end
+
+RPSGame.new.play
